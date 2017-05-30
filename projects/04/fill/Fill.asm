0ce663cabcd281accba0773699ec65a3eaa49d97
Invent Victor <victorshoaga@gmail.com>
2017-05-30 09:38:39 +0100

    feature: complete nand2Teris projects

---
+++ projects/04/fill/Fill.asm
@@ -0,0 +1,30 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
+
+// Put your code here.
+
+(BEGIN)
+@KBD
+D=M
+@DRAWBLACK
+D;JNE
+
+@0
+D=A
+@SCREEN
+M=D
+@BEGIN
+D;JNE
+
+(DRAWBLACK)
+@SCREEN
+M=-1
+@BEGIN
+0;JMP
